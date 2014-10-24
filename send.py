ed3d74c14689afcb3b8911f84da407eb3e3ee43a
Eric Song <eric.song@rutgers.edu>
2014-10-24 19:04:16 -0400

    added get data for send.py

--- send.py
+++ send.py
@@ -1,3 +1,11 @@
 from pymongo import MongoClient
+import sys
 
-
+sale_url = sys.argv[1]
+client = MongoClient('localhost', 27017)
+db = client.grandpateemo
+sales = db.sales
+sale_data = sales.find_one({'href': sale_url})
+for skin in sale_data['skins']:
+  
+print sale_data
