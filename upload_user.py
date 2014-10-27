39a6afe2db595bec0a0c41948d0af0600ee4fc26
Eric Song <eric.song@rutgers.edu>
2014-10-27 18:06:54 -0400

    created test user upload script

---
+++ upload_user.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.grandpateemo
+users = db.users
+
+#upload data
+email = 'regonics@gmail.com'
+wishlist = [35002, 62004, 24000, 5900]
+verified = True
+
+users.insert({
+  'email': email,
+  'wishlist': wishlist,
+  'verified': verified
+})
