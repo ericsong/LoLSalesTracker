103d0fc61228991d5fd4bd989fdcd6a45ab3d18c
Eric Song <regonics@gmail.com>
2014-10-29 23:55:59 -0400

    testing ssh

--- send.py
+++ send.py
@@ -6,6 +6,5 @@ client = MongoClient('localhost', 27017)
 db = client.skinsfarm
 sales = db.sales
 sale_data = sales.find_one({'href': sale_url})
-for skin in sale_data['skins']:
   
 print sale_data
