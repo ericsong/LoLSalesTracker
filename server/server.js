1b00c9cf890acada98fa82576e3befc2cbce5d8c
Eric Song <regonics@gmail.com>
2014-11-02 02:29:36 -0500

    added mongoose code

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser'),
     session = require('express-session'),
     http = require('http'),
     app = express(),
-    server = http.createServer(app);
+    server = http.createServer(app),
+    mongoose = require('mongoose');
 
 //express middleware setup
 app.use(express.static(__dirname + '/public'));
@@ -12,7 +13,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'jade');
 
+// database setup
+mongoose.connect('mongodb://localhost/lolwishlist');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function cb() {
+	console.log("mongoose connected");
+});
+
+// item schema
+var itemSchema = mongoose.Schema({
+	type: String,
+	name: String,
+	id: Number	
+});
+
+//
 //express functions
+//
 
 //return list of champion names
 app.get('/getChampions', function(req, res) {
