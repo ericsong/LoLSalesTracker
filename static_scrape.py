3be0d747a65cd17a763971d1aff22befc84c4663
Eric Song <regonics@gmail.com>
2014-11-04 19:48:24 -0500

    adding display names

--- static_scrape.py
+++ static_scrape.py
@@ -5,12 +5,14 @@ import requests
 
 LOL_API_KEY = os.environ['LOL_API_KEY']
 
-def insertChamp(name, id):
+def insertChamp(name, key, id):
   item = items.find_one({'id': id}) 
   if item is None:
     items.insert({
       'name': "".join(name.split()).lower(),
       'champ': "".join(name.split()).lower(),
+      'display_name': name,
+      'key': key,
       'id': id,
       'type': 'champ'
     })
@@ -21,6 +23,7 @@ def insertSkin(name, champ, id):
     items.insert({
       'name': "".join(name.split()).lower(),
       'champ': "".join(champ.split()).lower(),
+      'display_name': name,
       'id': id,
       'type': 'skin'
     })
@@ -37,6 +40,6 @@ for champ in champions:
   skins = champ_data['skins']
   for skin in skins:
     if skin['name'] == 'default':
-      insertChamp(champ_data['name'], skin['id'])
+      insertChamp(champ_data['name'], champ, skin['id'])
     else: 
       insertSkin(skin['name'], champ_data['name'], skin['id'])
