0be61d85d43faa1db94525a980a08a80ca75e1ce
Eric Song <eric.song@rutgers.edu>
2015-06-02 18:30:05 -0400

    uploads returned data to db

--- scrape_server.py
+++ scrape_server.py
@@ -1,13 +1,20 @@
+from pymongo import MongoClient
 import threading
 import scrape as LWL
 
+client = MongoClient('localhost', 27017)
+db = client.lolwishlist
+db_sales = db.sales
+
 def f():
   # do something here ...
-  data = LWL.checkForNewSales()
-  for sale in data:
+  sales = LWL.checkForNewSales()
+  for sale in sales:
+    #upload to database
     print sale['title']
     print sale['champs']
     print sale['skins']
+    db_sales.insert(sale);
 
   # call f() again in 60 seconds
   threading.Timer(60, f).start()
