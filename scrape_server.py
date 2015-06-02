e131591f45e34fffa91831f2db92ec0db6cd663d
Eric Song <eric.song@rutgers.edu>
2015-06-02 19:47:09 -0400

    writes and sends very basic email to me on new sales

--- scrape_server.py
+++ scrape_server.py
@@ -1,10 +1,18 @@
 from pymongo import MongoClient
 import threading
 import scrape as LWL
+import os
+import sendgrid
 
+#db setup
 client = MongoClient('localhost', 27017)
 db = client.lolwishlist
 db_sales = db.sales
+
+#sendgrid setup
+sg_username = os.environ['sg_username']
+sg_password = os.environ['sg_password']
+sg = sendgrid.SendGridClient(sg_username, sg_password)
 
 def f():
   # do something here ...
@@ -15,6 +23,28 @@ def f():
     print sale['champs']
     print sale['skins']
     db_sales.insert(sale);
+   
+    #generate email body
+    email_body = ""
+    email_body += "Sale from " + sale['title'] + "\n\n\n"
+
+    email_body += "Champions: " + "\n"
+    for champ in sale['champs']:
+      email_body += champ[0] + ": " + champ[1] + "\n"
+
+    email_body += "Skins: " + "\n"
+    for skin in sale['skins']:
+      email_body += skin[0] + ": " + skin[1] + "\n"
+
+    #generate email
+    message = sendgrid.Mail()
+    message.add_to('regonics@gmail.com')
+    message.set_subject('New League of Legends Skin/Champion sale!')
+    message.set_text(email_body)
+    message.set_from("LoLSalesTracker <tracker@lolsalestracker.com>")
+    status, msg = sg.send(message)
+
+    print status
 
   # call f() again in 60 seconds
   threading.Timer(60, f).start()
