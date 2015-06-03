dccd03c737a8bfd6e59910331b58c44a02abdd5b
Eric Song <eric.song@rutgers.edu>
2015-06-03 17:00:13 -0400

    finds users that need to be emailed

--- scrape_server.py
+++ scrape_server.py
@@ -2,52 +2,58 @@ from pymongo import MongoClient
 import threading
 import scrape as LWL
 import os
+import sys
 import sendgrid
 
 #db setup
 client = MongoClient('localhost', 27017)
 db = client.lolwishlist
 db_sales = db.sales
+db_items = db.items
+db_users = db.users
 
 #sendgrid setup
 sg_username = os.environ['sg_username']
 sg_password = os.environ['sg_password']
 sg = sendgrid.SendGridClient(sg_username, sg_password)
 
-def f():
-  # do something here ...
-  sales = LWL.checkForNewSales()
+#def generateEmailBody(items, sale):
+    #template = """<div dir=3D"ltr">An item from your wishlist, <b>Nightblade Irelia</b>, will be on sale for 400 RP from 6/02 ~ 6/05.<br><br>You will no longer receive email alerts when <b>Nightblade Irelia </b>goes on sale<b>. </b>If you wish to keep tracking this item, please <a href=3D"https://www.google.com">click here.</a><br><br><div><b>Champion and Skin Sales: 5/29 ~ 6/3</b></div><div>Zac - 440 RP</div><div>Swain - 440 RP</div><div>Miss Fortune - 395 RP</div><div>Shockblade Zed - 487 RP</div><div>Arcade Hecarim - 487 RP</div><div>Commando Jarvan IV - 487 RP<br><br><a href=3D"http://na.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-0526-0529">Official Link</a><br><br><a href=3D"http://www.google.com">Click here</a> to unsubscribe form this service.</div></div>"""
+
+def checkForNewSales():
+  #grab new sales
+  sales = LWL.getNewSales()
   for sale in sales:
     #upload to database
     print sale['title']
     print sale['champs']
     print sale['skins']
     db_sales.insert(sale);
-   
-    #generate email body
-    email_body = ""
-    email_body += "Sale from " + sale['title'] + "\n\n\n"
 
-    email_body += "Champions: " + "\n"
+    #generate item basket with item ids
+    saleIds = []
     for champ in sale['champs']:
-      email_body += champ[0] + ": " + champ[1] + "\n"
+      matched_item = db_items.find_one({"display_name": champ[0]})
+      if matched_item:
+        saleIds.append(matched_item['id'])
+      else:
+        print "no match error"
 
-    email_body += "Skins: " + "\n"
-    for skin in sale['skins']:
-      email_body += skin[0] + ": " + skin[1] + "\n"
+    for champ in sale['skins']:
+      matched_item = db_items.find_one({"display_name": champ[0]})
+      if matched_item:
+        saleIds.append(matched_item['id'])
+      else:
+        print "no match error"
 
-    #generate email
-    message = sendgrid.Mail()
-    message.add_to('regonics@gmail.com')
-    message.set_subject('New League of Legends Skin/Champion sale!')
-    message.set_text(email_body)
-    message.set_from("LoLSalesTracker <tracker@lolsalestracker.com>")
-    status, msg = sg.send(message)
-
-    print status
-
+    print saleIds
+    #find users that need to be emailed 
+    
+    matched_users = db_users.find({"wishlist": { "$in": saleIds }})
+    for user in matched_users:
+      print user
   # call f() again in 60 seconds
-  threading.Timer(60, f).start()
+  threading.Timer(60, checkForNewSales).start()
 
 # start calling f now and every 60 sec thereafter
-f()
+checkForNewSales()
